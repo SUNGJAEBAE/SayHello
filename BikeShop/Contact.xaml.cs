5493540f2fe77e8f88b640dac34eb67be4b20be2
sungjae bae <sungjae@gridwiz.com>
2021-04-07 21:15:20 +0900

    초기 커밋.

---
+++ BikeShop/Contact.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BikeShop
+{
+    /// <summary>
+    /// Contact.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Contact : Page
+    {
+        public Contact()
+        {
+            InitializeComponent();
+            var h = new Human();
+            h.FirstName = "Nick";
+            h.HasDrivingLiscense = true;
+
+            var c = new Car();
+            c.Speed = 100;
+            c.Color = Colors.Red;
+            c.Driver = h;
+        }
+        private void Greet(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello");
+
+        }
+    }
+}
